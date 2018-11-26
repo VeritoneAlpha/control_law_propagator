226feec80556ab895316dcd3b54e84ba1cbb7a6a
Jordan Makansi <jmakansi@veritone.com>
2018-11-26 14:41:28 -0800

    Fixed the remaining bug in the jupyter notebook.  Now, moving on to:     - implement u_rhs     -

--- sliding_window.py
+++ sliding_window.py
@@ -162,7 +162,7 @@ def propagate_dynamics(sliding_window_instance):
         # prepend initial condition for q and p for propagating u
         lhs_qp_vecs = [qpu_vec[:-1]] + qp_vecs[:-1] # last item in qpu_vec is "u", so leave it out. last item in qp_vecs is the last point in propagation (since we are using left hand side of q and p - leave it out.
         u_vecs = propagate_u(u_0, lhs_qp_vecs, t_start, t_end, sliding_window_instance)      # pass in the resulting lhs q and p values to be used for propagating the "u"
-        # t=0.0 doesn't matter what the value is here because derivative is not a function of time anyway (it's time invariant)
+        # again t=0.0 doesn't matter what the value is here because derivative is not a function of time anyway (it's time invariant)
         u_dot_vec = sliding_window_instance.u_rhs(0.0, u_vecs[-1], state_dim=sliding_window_instance.state_dim, Gamma = sliding_window_instance.Gamma, qp_vec = qp_vecs[:-1])
 
         qpu_vec_i = np.hstack([qp_vecs, u_vecs])
