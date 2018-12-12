8f1f22503ecd42fbc39427122426a4f99e2c57f7
Jordan Makansi <jmakansi@veritone.com>
2018-12-11 23:31:42 -0800

    Discovered a bug:    Recalling our notation, q_rhs_H_mf means the partial derivative of H_mf wrt q.  we are not doing that correctly inside of qp_rhs.

---
+++ agents_for_testing.py
@@ -0,0 +1,312 @@
+# Jordan Makansi
+# 11/18/18
+
+import numerical_propagator as prp
+
+import unittest   
+import numpy as np
+import abc
+import scipy as sp
+import ode
+
+from blackboard import *
+
+
+class Agent2:
+    
+    def __init__(self, blackboard, state_indices, control_indices):
+        '''
+        state_indices (list of integers): This list tells which states pertain to this agent. e.g. [1,2] would 
+        tell us that states 1 and 2 pertain to this agent.
+        
+        A word oNn notation:  The notation used for the methods of the agent is:  
+            - If it is a partial derivative: <denominator>_rhs_H_<type of hamiltonian (l, mf, or s)>_<nou or u>.  e.g., 
+            "qp_rhs_H_l_u" denotes the partial derivative with respect to q and p of the terms in the local Hamiltonian that contain control variables.
+            - If it is a hamiltonian: H_<type of hamiltonian (l, mf, or s)>_<nou or u>.  e.g. "H_mf_nou" denotes the mean field hamiltonian
+            with terms not containing u.
+        '''
+        self.state_indices = state_indices
+        self.control_indices = control_indices
+        self.bb = blackboard
+
+        # Inputs for numerical propagator
+        # qp_vec is going to be [q_s, p_l, p_mf], so it will have dimension = 3*state_dim
+
+        self.q_s_0 = np.array([0,2])
+        self.p_l_0 = np.array([0,3])
+        self.p_mf_0 = np.array([0,1])
+        self.u_s_0 = np.array([0])
+        self.qpu_vec = np.hstack([self.q_s_0, self.p_l_0, self.p_mf_0, self.u_s_0])
+        self.state_dim = 2
+        self.Gamma = 1 
+        self.gamma = 1 # gets computed each time the agent is visited
+        self.q_s_dot = np.array([0,1])  # must have same dimensions as q_s
+        self.sync = None # gets Synchronizer class is initialized
+        self.name='Agent2'
+        
+        # Inputs for numerical integration
+        self.integrateTol = 10**-5
+        self.integrateMaxIter = 400
+
+        # Inputs for sliding window
+        self.t_0 = 0
+        self.T = 2
+        self.K = 4
+
+        self.t_terminal = 2
+        self.n_s = 10
+
+        self.validate_dimensions()
+
+    def validate_dimensions(self):
+        # TODO: move to parent class "SlidingWindow"
+        assert len(self.state_indices) == self.state_dim, 'state dimensions are not consistent.  dimension of state indices is '+str(len(self.state_indices)) +' and state_dim is '+str(self.state_dim)
+        assert len(self.control_indices) == len(self.u_s_0), 'control dimensions are not consistent.  dimension of control_indices is '+str(len(self.control_indices)) +' and len(u_0) is '+str(len(self.u_s_0))
+        assert len(self.qpu_vec) == 3*self.state_dim + len(self.control_indices), ' control and state dimensions are not consistent with qpu_vec : length of qpu_vec is '+str(len(self.qpu_vec))+ ' and 3*self.state_dim + len(self.control_indices) is ' + str(3*self.state_dim + len(self.control_indices))
+    
+    '''
+    TODO:  
+    Add an assertion to check that the dimension of q_s_0, p_mf_0, and u_0:
+        - the dimension of state_dim    
+        - state_indices and control_indices set upon initiation of the Agent
+    '''
+    
+    def L_l(self, q_s, q_s_dot, u_s):
+        return 1
+    
+    def L_l_q_dot(self, q_s, q_s_dot, u_s):
+        return q_s
+    
+    def H_l_nou(self, q_s, p_l, lambda_l):
+        return 1
+
+    def H_l_u(self, q_s, p_l):
+        return np.array([0])
+
+    def q_rhs_H_l(self, q_s, p_l, u_s, lambda_l):
+        q_rhs_H_l_u = self.q_rhs_H_l_u(q_s, p_l)
+        q_rhs_H_l_u_summed = sum([q_rhs_H_l_u[i]*u_s[i] for i in range(len(u_s))])
+        return self.q_rhs_H_l_nou(q_s, p_l, lambda_l) + q_rhs_H_l_u_summed
+
+    def qp_rhs_H_l(self, q_s, p_l, u_s, lambda_l):
+        # TODO: there is one lambda_l per constraint. need to work out dimensions.
+        q_H_l_dot = self.p_rhs_H_l(q_s, p_l, u_s, lambda_l)
+        p_H_l_dot = self.q_rhs_H_l(q_s, p_l, u_s, lambda_l)
+        return np.concatenate([q_H_l_dot, p_H_l_dot])
+
+    def q_rhs_H_l_u(self, q_s, p_l):
+        # this must be 2D array, so wrap it in np.array
+        q_rhs_H_l_u = np.array([q_s])
+        return q_rhs_H_l_u
+    
+    def q_rhs_H_l_nou(self, q_s, p_l, lambda_l):
+        # this needs to be 1d array so no need to wrap it in np.array, unlike q_rhs_H_l_u
+        q_rhs_H_l_nou = q_s
+        return q_rhs_H_l_nou
+    
+    def p_rhs_H_l(self, q_s, p_l, u_s, lambda_l):
+        p_rhs_H_l_u = self.p_rhs_H_l_u(q_s, p_l)
+        p_rhs_H_l_u_summed = sum([p_rhs_H_l_u[i]*u_s[i] for i in range(len(u_s))])
+        return self.p_rhs_H_l_nou(q_s, p_l, lambda_l) + p_rhs_H_l_u_summed
+
+    def p_rhs_H_l_u(self, q_s, p_l):
+        # this must be 2D array, so wrap it in np.array
+        p_rhs_H_l_u = np.array([q_s])
+        return p_rhs_H_l_u
+    
+    def p_rhs_H_l_nou(self, q_s, p_l, lambda_l):
+        # this needs to be 1d array so no need to wrap it in np.array, unlike q_rhs_H_l_u
+        p_rhs_H_l_nou = q_s
+        return p_rhs_H_l_nou
+
+    def H_l(self, q_s, p_l, lambda_l, u_s):
+        # used in "Construct local Hamiltonian of agent i"
+        H_l_nou = self.H_l_nou(q_s, p_l, lambda_l)
+        H_l_u = self.H_l_u(q_s, p_l, lambda_l)
+        H_l_u_summed = sum([H_l_u[i]*u_s[i] for i in range(len(u_s))])
+        H_l = H_l_nou + H_l_u_summed
+        return H_l            
+    
+    def compute_lambdas(self, q_s, p_l, u_l):
+        # not implemented yet
+        return np.ones((1,self.state_dim))
+    
+    def H_l_D(self, q_lD, p_lD):
+        return np.array(q_lD).dot(p_lD)
+        
+    def L_l_D(self, q_lD, p_lD):
+        # return scalar
+        return 1
+        
+    def L_l_D_q_Dot(self, q_lD, p_lD):
+        # return 1 by state_dim, 1-D array
+        # each q_lD is a 1-D array of size 1 by state_dim array
+        return 1
+    
+    def qp_rhs(self, t, qp_vec, **kwargs):
+        # u_s is constant (because of causality, remember?)
+        u_s = kwargs['u_0']
+        state_dim = kwargs['state_dim']
+        q_mf = kwargs['q_mf']
+        u_mf = kwargs['u_mf']
+        
+        # TODO:  get a kwargs working for lambda_l
+        lambda_l = 0 # kwargs['lambda_l']
+        q_s = qp_vec[:state_dim]
+        p_l = qp_vec[state_dim:2*state_dim]
+        p_mf = qp_vec[2*state_dim:]
+
+        qp_rhs_H_mf = self.qp_rhs_H_mf(q_mf, p_mf, u_mf, u_s)
+        p_rhs_H_mf = qp_rhs_H_mf[:state_dim]
+        q_rhs_H_mf = qp_rhs_H_mf[state_dim:]
+        
+        qp_rhs_H_l = self.qp_rhs_H_l(q_s, p_l, u_s, lambda_l)
+        q_rhs_H_l = qp_rhs_H_l[:state_dim]
+        p_rhs_H_l = qp_rhs_H_l[state_dim:]
+
+        q_s_dot = self.gamma*q_rhs_H_mf + (1-self.gamma)*q_rhs_H_l
+        p_mf_dot = p_rhs_H_mf
+        p_l_dot = -1*p_rhs_H_l
+
+        return np.concatenate([q_s_dot, p_l_dot, p_mf_dot])
+    
+    def u_rhs(self, t, u_vec, **kwargs):
+        u_s = kwargs['u_0']
+        state_dim = kwargs['state_dim']
+        q_mf_dot = kwargs['q_mf_dot']
+        q_s_dot = kwargs['q_s_dot']
+        p_mf_dot = kwargs['p_mf_dot']
+        q_mf = kwargs['q_mf']
+        u_mf = kwargs['u_mf']
+        qp_vec = kwargs['qp_vec']
+        H_l_D = kwargs['H_l_D']
+        q_s = qp_vec[:state_dim]
+        p_l = qp_vec[state_dim:2*state_dim]
+        p_mf = qp_vec[2*state_dim:]
+
+        def Beta_j(self, q_mf, p_mf, u_mf, u_s, q_s_dot, q_mf_dot, p_mf_dot, q_s, p_l, j):
+            Beta_mf=[]
+            Beta_l=[]
+            H_mf_u = self.H_MF_u(q_mf, p_mf)
+            H_l_u = self.H_l_u(q_s, p_l)
+            lambda_l=0
+            j=j-1 # indices start at 0 but control indices start at 1
+            for k in range(len(self.control_indices)):
+                Beta_mf_k = H_mf_u[j]*(np.dot(self.q_rhs_H_mf_u(p_mf, q_mf, u_mf)[k], q_s_dot) + np.dot(self.p_rhs_H_mf_u(p_mf, q_mf, u_mf)[k], p_mf_dot)) + \
+                            H_mf_u[k]*(np.dot(self.q_rhs_H_mf_u(p_mf, q_mf, u_mf)[j], q_s_dot) + np.dot(self.p_rhs_H_mf_u(p_mf, q_mf, u_mf)[j], p_mf_dot))
+                Beta_l_k = H_l_u[j]*(np.dot(self.q_rhs_H_l_u(q_s, p_l)[k], q_s_dot) + np.dot(self.p_rhs_H_l_u(q_s, p_l)[k], p_mf_dot)) + \
+                           H_l_u[k]*(np.dot(self.q_rhs_H_l_u(q_s, p_l)[j], q_s_dot) + np.dot(self.p_rhs_H_l_u(q_s, p_l)[j], p_mf_dot))
+                    
+                Beta_mf.append(Beta_mf_k)
+                Beta_l.append(Beta_l_k)
+            return Beta_mf, Beta_l
+                
+        def alpha_j(self, q_mf, p_mf, u_mf, u_s, q_s_dot, q_mf_dot, p_mf_dot, q_s, p_l, H_l_D, j):
+            alpha_mf = []
+            alpha_l = []
+            H_mf_u = self.H_MF_u(q_mf, p_mf)
+            H_l_u = self.H_l_u(q_s, p_l)
+            j=j-1
+            H_mf_nou = self.H_MF_nou(q_mf, p_mf, u_mf)
+            H_l_nou = self.H_l_nou(q_mf, p_mf, u_mf)
+            for k in range(len(self.control_indices)):
+                alpha_mf_k = H_mf_u[j]*(np.dot(self.q_rhs_H_mf_u(p_mf, q_mf, u_mf)[k], q_s_dot) + np.dot(self.p_rhs_H_mf_u(p_mf, q_mf, u_mf)[k], p_mf_dot)) +\
+                            (H_mf_nou-H_l_D)*(np.dot(self.q_rhs_H_mf_u(p_mf, q_mf, u_mf)[j], q_s_dot) + np.dot(self.p_rhs_H_mf_u(p_mf, q_mf, u_mf)[j], p_mf_dot))
+
+                alpha_l_k = H_l_u[j]*(np.dot(self.q_rhs_H_l_u(q_s, p_l)[k], q_s_dot) + np.dot(self.p_rhs_H_l_u(q_s, p_l)[k], p_l_dot)) +\
+                            (H_l_nou-H_l_D)*(np.dot(self.q_rhs_H_l_u(q_s, p_l)[j], q_s_dot) + np.dot(self.p_rhs_H_l_u(q_s, p_l)[j], p_l_dot))
+                alpha_mf.append(alpha_mf_k)
+                alpha_l.append(alpha_l_k)
+            return alpha_mf, alpha_l
+                
+        for j in self.control_indices:
+            Beta_mf, Beta_l = Beta_j(self, q_mf, p_mf, u_mf, u_s, q_s_dot, q_mf_dot, p_mf_dot, q_s, p_l, j)
+            alpha_mf, alpha_l = alpha_j(self, q_mf, p_mf, u_mf, u_s, q_s_dot, q_mf_dot, p_mf_dot, q_s, p_l, H_l_D, j)
+
+        print Beta_mf, Beta_l
+        print alpha_mf, alpha_l
+        return -1*self.Gamma*(self.gamma*(alpha_mf + np.dot(Beta_mf,u_s)) +\
+                              (1-self.gamma)*(alpha_l + np.dot(Beta_l,u_s)))
+
+
+    ## Mean Field methods
+    def H_MF_nou(self, q_mf, p_mf, u_mf):
+        return 1
+
+    def H_MF_u(self, q_mf, p_mf):
+        # q_mf, u_mf are vectors for ALL of the states, and controls
+        # retrns a numpy array with each element corresponding to H_mf for a particular control variable, j
+        return np.array([self.H_MF_u_1(q_mf, p_mf)]) #*u_mf[0]])  # + self.H_MF_u_2(q_mf, p_mf)*u_mf[1]
+
+    def H_MF_u_1(self, q_mf, p_mf):
+        return q_mf[0]*q_mf[1]
+    
+#     def H_MF_u_2(self, q_mf, p_mf):
+#         return q_mf[1]
+        
+    def qp_rhs_H_mf(self, q_mf, p_mf, u_mf, u_s):
+        # remember that we want to propagate as much as possible together in the same rhs function for numerical purposes
+        # remember that q_rhs here is w.r.t p_mf but p_rhs here is w.r.t q_s
+        q_H_mf_dot = self.p_rhs_H_mf(q_mf, p_mf, u_mf, u_s)
+        p_H_mf_dot = self.q_rhs_H_mf(q_mf, p_mf, u_mf, u_s)
+        return np.concatenate([q_H_mf_dot, p_H_mf_dot])
+    
+    def q_rhs_H_mf(self, q_mf, p_mf, u_mf, u_s):
+        # q_rhs_H_mf is the derivative wrt each of the local variables, so it will return something of dimension state_dim
+        # q_rhs_H_mf_u returns the partial derivatives wrt each control, concatenated together
+        q_rhs_H_mf_u = self.q_rhs_H_mf_u(q_mf, p_mf, u_mf)
+        assert np.shape(q_rhs_H_mf_u)==(len(self.control_indices), self.state_dim) # first dimension should be number of controls, inner dimension should be state_dim
+        q_rhs_H_mf_u_summed = sum([q_rhs_H_mf_u[i]*u_s[i] for i in range(len(u_s))])
+        return self.q_rhs_H_mf_nou(q_mf, p_mf) + q_rhs_H_mf_u_summed
+        
+    def q_rhs_H_mf_u(self, q_mf, p_mf, u_mf):
+        # this method is will return a concatenation of all of the partial derivatives for each of the controls
+        # each of the partial derivatives is of dimension state_dim
+        # this means that this method will return a 2D array:
+        #    - first dimension is the index of the control
+        #    - second dimension is the index of the state
+        p_H_mf_u_dot_1 = p_mf # must be of dimension state_dim
+        q_rhs_H_mf_u = np.array([p_H_mf_u_dot_1])
+        return q_rhs_H_mf_u
+
+    def p_rhs_H_mf(self, q_mf, p_mf, u_mf, u_s):
+        # q_rhs_H_mf is the derivative wrt each of the local variables, so it will return something of dimension state_dim
+        # q_rhs_H_mf_u returns the partial derivatives wrt each control, concatenated together
+        p_rhs_H_mf_u = self.p_rhs_H_mf_u(q_mf, p_mf, u_mf)
+        p_rhs_H_mf_u_summed = sum([p_rhs_H_mf_u[i]*u_s[i] for i in range(len(u_s))])
+        return self.p_rhs_H_mf_nou(q_mf, p_mf) + p_rhs_H_mf_u_summed
+        
+    def p_rhs_H_mf_nou(self, q_mf, p_mf):
+        return  p_mf # or something
+
+    def q_rhs_H_mf_nou(self, q_mf, p_mf):
+        return  p_mf
+
+    def p_rhs_H_mf_u(self, q_mf, p_mf, u_mf):
+        q_H_mf_u_dot = p_mf
+        return np.array([q_H_mf_u_dot])
+
+    def L_mf_q_dot(self, q_mf, q_mf_dot, u_mf):
+        # q_mf_dot, q_mf (inputs) here will be vectors with ALL of the states
+        # u_mf is a vector of ALL of the controls
+        # extract q_s from q_mf
+        
+        # note that these methods must return vectors that are of local dimension - state_dim - even though they take in vectors of dimension for all the states
+        # the user needs to be aware of the indices the correspond to each state
+
+        def L_l_q_dot_agent_1(q_mf, q_mf_dot, u_mf):
+            return np.concatenate([np.array([q_mf[0]]),np.array([q_mf[1]])])
+        
+        def L_l_q_dot_agent_2(q_mf, q_mf_dot, u_mf):
+            return np.concatenate([np.array([q_mf[0]]),np.array([q_mf[1]])])
+        
+        L_mf_total_q_dot = np.zeros(self.state_dim)
+
+        # agent 1
+        L_mf_total_q_dot += L_l_q_dot_agent_1(q_mf, q_mf_dot, u_mf)
+
+        # agent 2
+        L_mf_total_q_dot += L_l_q_dot_agent_2(q_mf, q_mf_dot, u_mf)
+
+        assert np.shape(L_mf_total_q_dot)[0] == self.state_dim, 'dimensions of L_mf_total_q_dot must match those of the local state, currently the dimensions are ' +str(np.shape(L_mf_total_q_dot)[0])
+        return L_mf_total_q_dot
