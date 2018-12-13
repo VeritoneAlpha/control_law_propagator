Fixed and tested the beta and alpha functions in the notebook, now just need to propagate those changes into numerical propagator.py.
The methods for computing beta and alpha inside u_rhs should live outside
of the ode method because they only need to be evaluated once. As such,
they will be moved outisde of the ode method and into the library of
helper functions inside of numerical propagator.py.

agents_for_testing.py
@@ -184,44 +184,12 @@ class Agent1:
         qp_vec = kwargs['qp_vec']
         H_l_D = kwargs['H_l_D']
         p_l_dot = kwargs['p_l_dot']
+        #beta_mf = kwargs['beta_mf']
+        #alpha_mf = kwargs['alpha_mf']
         q_s = qp_vec[:state_dim]
         p_l = qp_vec[state_dim:2*state_dim]
         p_mf = qp_vec[2*state_dim:]
 
-        def Beta_j(self, q_mf, p_mf, u_mf, u_s, q_s_dot, q_mf_dot, p_mf_dot, q_s, p_l, j):
-            Beta_mf=[]
-            Beta_l=[]
-            H_mf_u = self.H_MF_u(q_mf, p_mf)
-            H_l_u = self.H_l_u(q_s, p_l)
-            lambda_l=0
-            j=j-1 # indices start at 0 but control indices start at 1
-            for k in range(len(self.control_indices)):
-                Beta_mf_k = H_mf_u[j]*(np.dot(self.q_rhs_H_mf_u(p_mf, q_mf, u_mf)[k], q_s_dot) + np.dot(self.p_rhs_H_mf_u(p_mf, q_mf, u_mf)[k], p_mf_dot)) + \
-                            H_mf_u[k]*(np.dot(self.q_rhs_H_mf_u(p_mf, q_mf, u_mf)[j], q_s_dot) + np.dot(self.p_rhs_H_mf_u(p_mf, q_mf, u_mf)[j], p_mf_dot))
-                Beta_l_k = H_l_u[j]*(np.dot(self.q_rhs_H_l_u(q_s, p_l)[k], q_s_dot) + np.dot(self.p_rhs_H_l_u(q_s, p_l)[k], p_mf_dot)) + \
-                           H_l_u[k]*(np.dot(self.q_rhs_H_l_u(q_s, p_l)[j], q_s_dot) + np.dot(self.p_rhs_H_l_u(q_s, p_l)[j], p_mf_dot))
-                    
-                Beta_mf.append(Beta_mf_k)
-                Beta_l.append(Beta_l_k)
-            return Beta_mf, Beta_l
-                
-        def alpha_j(self, q_mf, p_mf, u_mf, u_s, q_s_dot, q_mf_dot, p_mf_dot, q_s, p_l, H_l_D, j):
-            alpha_mf = []
-            alpha_l = []
-            H_mf_u = self.H_MF_u(q_mf, p_mf)
-            H_l_u = self.H_l_u(q_s, p_l)
-            j=j-1
-            H_mf_nou = self.H_MF_nou(q_mf, p_mf, u_mf)
-            H_l_nou = self.H_l_nou(q_mf, p_mf, u_mf)
-            for k in range(len(self.control_indices)):
-                alpha_mf_k = H_mf_u[j]*(np.dot(self.q_rhs_H_mf_u(p_mf, q_mf, u_mf)[k], q_s_dot) + np.dot(self.p_rhs_H_mf_u(p_mf, q_mf, u_mf)[k], p_mf_dot)) +\
-                            (H_mf_nou-H_l_D)*(np.dot(self.q_rhs_H_mf_u(p_mf, q_mf, u_mf)[j], q_s_dot) + np.dot(self.p_rhs_H_mf_u(p_mf, q_mf, u_mf)[j], p_mf_dot))
-
-                alpha_l_k = H_l_u[j]*(np.dot(self.q_rhs_H_l_u(q_s, p_l)[k], q_s_dot) + np.dot(self.p_rhs_H_l_u(q_s, p_l)[k], p_l_dot)) +\
-                            (H_l_nou-H_l_D)*(np.dot(self.q_rhs_H_l_u(q_s, p_l)[j], q_s_dot) + np.dot(self.p_rhs_H_l_u(q_s, p_l)[j], p_l_dot))
-                alpha_mf.append(alpha_mf_k)
-                alpha_l.append(alpha_l_k)
-            return alpha_mf, alpha_l
                 
         for j in self.control_indices:
             Beta_mf, Beta_l = Beta_j(self, q_mf, p_mf, u_mf, u_s, q_s_dot, q_mf_dot, p_mf_dot, q_s, p_l, j)