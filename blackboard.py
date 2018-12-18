453d2ff2269ce8ceb7c09e266940e94065627d75
Jordan Makansi <jmakansi@veritone.com>
2018-12-17 21:07:05 -0800

    Fixed the blackboard q_p_u_dict so that the entries are appropriately called q_mf, q_mf_dot, and u_mf. Tests pass. Now, going to continue working on main.py reading from the config, and running sliding_window on a single agent.

--- blackboard.py
+++ blackboard.py
@@ -26,7 +26,7 @@ class Blackboard:
             - holds all of the agents in entire system
         '''
         ## TODO:  _s should really be called _mf because it contains all of the states/controls.
-        self.q_p_u_dict = {'q_mf':{}, 'p_l':{}, 'p_mf':{}, 'u_s':{}, 'q_mf_dot':{}}
+        self.q_p_u_dict = {'q_mf':{}, 'p_l':{}, 'p_mf':{}, 'u_mf':{}, 'q_mf_dot':{}}
         self.bar_dict = {}
         self.agents=[]
        
@@ -53,10 +53,10 @@ class Blackboard:
             self.q_p_u_dict['q_mf'][str(state_ix)] = agent.qpu_vec[:agent.state_dim][state_ix-1]
             self.q_p_u_dict['p_l'][str(state_ix)] = agent.qpu_vec[agent.state_dim:2*agent.state_dim][state_ix-1]
             self.q_p_u_dict['p_mf'][str(state_ix)] = agent.qpu_vec[2*agent.state_dim:3*agent.state_dim][state_ix-1]
-            self.q_p_u_dict['q_mf_dot'][str(state_ix)] = agent.q_mf_dot[state_ix-1]
+            self.q_p_u_dict['q_mf_dot'][str(state_ix)] = agent.q_s_dot[state_ix-1]
             
         for control_ix in agent.control_indices:
-            self.q_p_u_dict['u_s'][str(control_ix)] = agent.qpu_vec[3*agent.state_dim:][control_ix-1]
+            self.q_p_u_dict['u_mf'][str(control_ix)] = agent.qpu_vec[3*agent.state_dim:][control_ix-1]
        
         # add agent if not already added 
         if agent not in self.agents:
@@ -83,7 +83,7 @@ class Blackboard:
    
 def construct_mf_vectors(sliding_window_instance):
     '''
-    Helper function to get q_mf, q_mf_dot, u_mf (incorrectly called q_s, q_s_dot, u_s, in the q_p_u_dict)
+    Helper function to get q_mf, q_mf_dot, u_mf. 
     Inputs:
         sliding_window_instance (object of class SlidingWindow)
     Ouput:
@@ -98,7 +98,7 @@ def construct_mf_vectors(sliding_window_instance):
     bb = sliding_window_instance.bb
     qpu_vec = sliding_window_instance.qpu_vec
     q_mf_shape = len(bb.q_p_u_dict['q_mf'].items())
-    u_mf_shape = len(bb.q_p_u_dict['u_s'].items())
+    u_mf_shape = len(bb.q_p_u_dict['u_mf'].items())
     q_mf = np.zeros(q_mf_shape)  # this should be an array of values
     q_mf_dot = np.zeros(q_mf_shape)  # this should be an array of values
     u_mf = np.zeros(u_mf_shape) # this should be an array of values
@@ -118,7 +118,7 @@ def construct_mf_vectors(sliding_window_instance):
             # fill in q_mf with value from blackboard
             q_mf[int(q_ix)-1] = q_val
 
-    for u_ix, u_val in bb.q_p_u_dict['u_s'].items():
+    for u_ix, u_val in bb.q_p_u_dict['u_mf'].items():
         # if this index does NOT PERTAIN to this agent, then fill in q_mf with value from the blackboard
         # if the index does PERTAIN to this agent, then fill in q_mf with the value from qpu_vec
         if int(u_ix) in sliding_window_instance.control_indices:
@@ -130,7 +130,7 @@ def construct_mf_vectors(sliding_window_instance):
             # fill in q_mf with value from blackboard
             u_mf[int(u_ix)-1] = u_val
 
-    for q_dot_ix, q_dot_val in bb.q_p_u_dict['q_s_dot'].items(): # if this index does NOT PERTAIN to this agent, then fill in q_mf with value from the blackboard
+    for q_dot_ix, q_dot_val in bb.q_p_u_dict['q_mf_dot'].items(): # if this index does NOT PERTAIN to this agent, then fill in q_mf with value from the blackboard
         # if the index does PERTAIN to this agent, then fill in q_mf with the value from qpu_vec
         if int(q_dot_ix) in sliding_window_instance.state_indices:
             # fill in q_mf with value from qpu_vec
@@ -171,7 +171,7 @@ def construct_local_vectors(sliding_window_instance):
             # TODO: write an assertion to make sure qpu_ix has exactly 1 element (not 0, and not more than 1)
             q_s[int(q_ix)-1] = qpu_vec[qpu_ix[0][0]]
 
-    for u_ix, u_val in bb.q_p_u_dict['u_s'].items():
+    for u_ix, u_val in bb.q_p_u_dict['u_mf'].items():
         if int(u_ix) in sliding_window_instance.control_indices:
             qpu_ix = np.where(np.array(sliding_window_instance.control_indices)==int(u_ix))
             u_s[int(u_ix)-1] = qpu_vec[qpu_ix[0][0]]
