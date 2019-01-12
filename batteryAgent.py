-	There is a dimension issue in getting q_s_dot.  For some reason qp_rhs seems to be retuning q_s_dot as the wrong dimensions

batteryAgent.py
@@ -175,10 +175,9 @@ class batteryAgent:
         q_B_dot = q_s_dot[1]
 
         u_B = u_s
-
         p_1 = (q_B_dot+q_1_dot)*self.R_1*delta
         p_B = q_B_dot*(self.R_0+self.R_1)*delta + q_1_dot*self.R_1*delta
-        return np.concatenate([np.array([p_1]), np.array([p_B])])
+        return np.array([p_1, p_B])
 
     def L_mf_q_dot(self, q_mf, q_mf_dot, u_mf):
         # q_mf_dot, q_mf (inputs) here will be vectors with ALL of the states
@@ -211,7 +210,8 @@ class batteryAgent:
     def H_l(self, q_s, p_l, u_s, lambda_l):
         # call using q_s, p_l, lambda_l
         H_l_nou = self.H_l_nou(q_s, p_l, lambda_l) 
-        H_l = H_l_nou + np.dot(H_l_nou, u_s)
+        H_l_u = self.H_l_u(q_s, p_l)
+        H_l = H_l_nou + np.dot(H_l_u, u_s)
         return H_l
 
     def H_l_nou(self, q_s, p_l, lambda_l):
@@ -242,10 +242,11 @@ class batteryAgent:
         return term_1 + term_2 + term_3 + term_4 + term_5
 
     def H_l_u(self, q_s, p_l):
+        # this returns a 1D numpy array, of dimension control_dim  
         q_1, q_B = q_s[0], q_s[1]
         q_B_0 = self.q_B_0
         term_1 = 0.5*(-(q_B - q_B_0))**2 
-        return term_1
+        return np.array([term_1])
 
     def q_rhs_H_l_nou(self, q_s, p_l, lambda_l):
         # TODO: replace as a function of self.K and self.T

numerical_propagator.py
@@ -322,12 +322,13 @@ def Betas(self, q_mf, p_mf, u_mf, u_s, q_s_dot, q_mf_dot, p_mf_dot, q_s, p_l):
         Beta_mf_j=np.array([])
         Beta_l_j=np.array([])
         for k in range(len(self.control_indices)):
+            import pdb; pdb.set_trace()
             # recall self.q_rhs_H_mf_u() returns a 2D np.array of size control_dim x state_dim, so self.q_rhs_H_mf_u()[k] is actually a 1D np.array
             Beta_mf_k = H_mf_u[j]*(np.dot(self.q_rhs_H_mf_u(p_mf, q_mf, u_mf)[k], q_s_dot) + np.dot(self.p_rhs_H_mf_u(p_mf, q_mf, u_mf)[k], p_mf_dot)) + \
                         H_mf_u[k]*(np.dot(self.q_rhs_H_mf_u(p_mf, q_mf, u_mf)[j], q_s_dot) + np.dot(self.p_rhs_H_mf_u(p_mf, q_mf, u_mf)[j], p_mf_dot))
             Beta_l_k = H_l_u[j]*(np.dot(self.q_rhs_H_l_u(q_s, p_l)[k], q_s_dot) + np.dot(self.p_rhs_H_l_u(q_s, p_l)[k], p_mf_dot)) + \
                        H_l_u[k]*(np.dot(self.q_rhs_H_l_u(q_s, p_l)[j], q_s_dot) + np.dot(self.p_rhs_H_l_u(q_s, p_l)[j], p_mf_dot))
-    
+            import pdb; pdb.set_trace() 
             Beta_mf_j=np.concatenate([Beta_mf_j, np.array([Beta_mf_k])])
             Beta_l_j=np.concatenate([Beta_l_j, np.array([Beta_l_k])])
         Beta_mf.append(Beta_mf_j) 
@@ -348,7 +349,6 @@ def alphas(self, q_mf, p_mf, u_mf, u_s, q_s_dot, q_mf_dot, p_mf_dot, q_s, p_l, H
         lambda_l=0
         alpha_mf_j = H_mf_u[j]*(np.dot(self.q_rhs_H_mf_nou(q_mf, p_mf), q_s_dot) + np.dot(self.p_rhs_H_mf_nou(q_mf, p_mf), p_mf_dot)) +\
                         (H_mf_nou-H_l_D)*(np.dot(self.q_rhs_H_mf_u(q_mf, p_mf, u_mf)[j], q_s_dot) + np.dot(self.p_rhs_H_mf_u(q_mf, p_mf, u_mf)[j], p_mf_dot))
-    
         alpha_l_j = H_l_u[j]*(np.dot(self.q_rhs_H_l_nou(q_s, p_l, lambda_l), q_s_dot) + np.dot(self.p_rhs_H_l_nou(q_s, p_l, lambda_l), p_l_dot)) +\
                         (H_l_nou-H_l_D)*(np.dot(self.q_rhs_H_l_u(q_s, p_l)[j], q_s_dot) + np.dot(self.p_rhs_H_l_u(q_s, p_l)[j], p_l_dot))
         alpha_mf.append(alpha_mf_j)